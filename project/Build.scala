0a4403b56cef981a1b856fef9ade30fb230d185f
shikhar <shikhar@schmizz.net>
2014-05-07 10:39:46 +0530

    Build file org

--- project/Build.scala
+++ project/Build.scala
@@ -7,21 +7,30 @@ object Build extends sbt.Build {
   lazy val eskka = Project(
     id = "eskka",
     base = file("."),
-    settings = standardSettings
+    settings = Defaults.defaultSettings
   ).settings(
-      mainClass := Some("eskka.Boot"),
+      organization := "eskka",
+      scalaVersion := "2.11.0",
+      version := "0.2.0-SNAPSHOT",
+      scalacOptions := Seq("-unchecked", "-deprecation", "-feature", "-encoding", "utf8")
+    ).settings(
+      SbtScalariform.ScalariformKeys.preferences in Compile := formattingPreferences,
+      SbtScalariform.ScalariformKeys.preferences in Test := formattingPreferences
+    ).settings(
+      packTask
+    ).settings(
 
       resolvers ++= Seq(Resolver.sonatypeRepo("releases"), Resolver.typesafeRepo("releases")),
 
       libraryDependencies ++= Seq(
-
-        "com.google.guava" % "guava" % "16.0.1",
 
         "org.elasticsearch" % "elasticsearch" % v.elasticsearch % "provided",
 
         "com.typesafe.akka" %% "akka-actor" % v.akka,
         "com.typesafe.akka" %% "akka-contrib" % v.akka,
         "com.typesafe.akka" %% "akka-cluster" % v.akka,
+
+        "com.google.guava" % "guava" % v.guava,
 
         "com.typesafe.akka" %% "akka-multi-node-testkit" % v.akka % "test"
 
@@ -30,13 +39,6 @@ object Build extends sbt.Build {
 
   lazy val pack = TaskKey[File]("pack")
 
-  def standardSettings = Defaults.defaultSettings ++ Seq(
-    organization := "eskka",
-    scalaVersion := "2.11.0",
-    version := "0.2.0-SNAPSHOT",
-    scalacOptions := Seq("-unchecked", "-deprecation", "-feature", "-encoding", "utf8")
-  ) ++ packTask ++ formatSettings
-
   def packTask = pack <<= (name, update, packageBin in Compile, target, version) map {
     (name, updateReport, jar, out, v) =>
       val archive = out / s"$name-$v.zip"
@@ -44,21 +46,17 @@ object Build extends sbt.Build {
       archive
   }
 
-  object v {
-    val elasticsearch = "1.1.1"
-    val akka = "2.3.2"
-  }
-
-  lazy val formatSettings = SbtScalariform.scalariformSettings ++ Seq(
-    SbtScalariform.ScalariformKeys.preferences in Compile  := formattingPreferences,
-    SbtScalariform.ScalariformKeys.preferences in Test     := formattingPreferences
-  )
-
   def formattingPreferences = {
     import scalariform.formatter.preferences._
     FormattingPreferences()
       .setPreference(AlignParameters, true)
       .setPreference(AlignSingleLineCaseStatements, true)
+  }
+
+  object v {
+    val elasticsearch = "1.1.1"
+    val akka = "2.3.2"
+    val guava = "16.0.1"
   }
 
 }
