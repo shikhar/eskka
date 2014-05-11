5fce5123bcb7b7be456c96a7bc21d01dd3a61d12
shikhar <shikhar@schmizz.net>
2014-05-11 12:09:24 +0530

    a TODO

--- src/main/scala/eskka/Follower.scala
+++ src/main/scala/eskka/Follower.scala
@@ -78,6 +78,9 @@ class Follower(localNode: DiscoveryNode, votingMembers: VotingMembers, clusterSe
 
           case Success(updatedState) =>
             require(updatedState.nodes.masterNodeId != localNode.id, "Master's local follower should not receive Publish messages")
+
+            //updatedState.status(ClusterState.ClusterStateStatus.RECEIVED) -- TODO upon upgrade 1434f6bcbb9cdfdd42f696aedfa199aab09b8ca2
+
             log.info("submitting publish of cluster state version {}...", updatedState.version)
             SubmitClusterStateUpdate(clusterService, "follower{master-publish}", Priority.URGENT, updateClusterState(updatedState)) onComplete {
               res =>
